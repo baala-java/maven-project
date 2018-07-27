7d3672135d6b5f737f9a0eb43ad8e256acb27797
baala-java <baala.java@gmail.com>
2018-07-28 01:08:54 +1200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,7 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    System.out.println("Test  dfsdfsfsd");
+    System.out.println("Test  dfsdfsfsd zxczxcxz");
     return String.format("Hello, %s!", someone);
   }
 }
